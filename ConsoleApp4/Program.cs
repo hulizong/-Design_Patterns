aba372e2811c95dc603617e1504b48024ec8a1c3
hulizong <a1207988466@qq.com>
2019-08-23 14:23:40 +0800

    设计模式单例模式事例

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+          var Get= Singleton4.Instance.GetString();
+            Console.WriteLine(Get);
+            Console.ReadLine();
+        }
+    }
+}
