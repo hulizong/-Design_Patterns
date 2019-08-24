7d46936e67ded7cf1e6be843f0e2a8554337e450
hulizong <a1207988466@qq.com>
2019-08-24 14:24:28 +0800

    工厂模式

---
+++ Factory Method/ConsoleApp4/Factory_Method_Pattern.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+   public  class Factory_Method_Pattern
+    {
+    }
+    #region  产品 =========================
+    /// <summary>
+    /// 抽象手机产品类
+    /// </summary>
+    public abstract class Phone
+    {
+        public abstract string Create();
+    }
+
+    /// <summary>
+    /// 具体华为手机产品类
+    /// </summary>
+    public class Huawei : Phone
+    {
+        /// <summary>
+        /// 实现抽象方法
+        /// </summary>
+        /// <returns></returns>
+        public override string Create()
+        {
+            return "华为一号现世";
+        }
+    }
+
+    /// <summary>
+    /// 具体小米手机产品类
+    /// </summary>
+    public class Xiaomi : Phone
+    {
+        /// <summary>
+        /// 实现抽象方法
+        /// </summary>
+        /// <returns></returns>
+        public override string Create()
+        {
+            return "小米一号现世";
+        }
+    }
+
+    #endregion
+
+    #region  工厂=========================
+    /// <summary>
+    /// 抽象工厂类
+    /// </summary>
+    public abstract class Factory
+    {
+        /// <summary>
+        /// 抽象工厂类的方法，创建调用产品
+        /// </summary>
+        /// <returns></returns>
+        public abstract Phone CreatePhone();
+    }
+
+    /// <summary>
+    /// 具体华为工厂类，继承抽象工厂类
+    /// </summary>
+    public class HuaweiFactory : Factory
+    {
+        /// <summary>
+        /// 实现继承方法
+        /// </summary>
+        /// <returns></returns>
+        public override Phone CreatePhone()
+        {
+            return new Huawei();
+        }
+    }
+
+    /// <summary>
+    /// 具体小米工厂类，继承抽象工厂类
+    /// </summary>
+    public class XiaomiFactory : Factory
+    {
+        /// <summary>
+        /// 实现继承方法
+        /// </summary>
+        /// <returns></returns>
+        public override Phone CreatePhone()
+        {
+            return new Xiaomi();
+        }
+    }
+    #endregion
+
+        #region 新增魅族手机 ================
+        /// <summary>
+        /// 新增魅族工厂类
+        /// </summary>
+        public class MeizuFactory : Factory
+        {
+            public override Phone CreatePhone()
+            {
+                return new Meizu();
+            }
+        }
+
+        /// <summary>
+        /// 新增具体魅族手机产品类
+        /// </summary>
+        public class Meizu : Phone
+        {
+            public override string Create()
+            {
+                return "魅族一号现世";
+            }
+        }
+        #endregion
+}
---
+++ Factory Method/ConsoleApp4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //初始化工厂
+            Factory huaweiFactory = new HuaweiFactory();
+            
+            //生产具体的华为手机
+            var result=huaweiFactory.CreatePhone();
+            //华为手机现世
+            var answer = result.Create();
+            Console.WriteLine(answer);
+
+            Factory xiaomiFactory = new XiaomiFactory();
+            result = xiaomiFactory.CreatePhone();
+            answer = result.Create();
+            Console.WriteLine(answer);
+
+            Factory meizuFactory = new MeizuFactory();
+            result = meizuFactory.CreatePhone();
+            answer = result.Create();
+            Console.WriteLine(answer);
+
+            Console.ReadLine();
+        }
+    }
+}
