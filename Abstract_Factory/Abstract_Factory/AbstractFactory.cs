11e1389a1809b5e155ccb72e610629fb7cbd7104
hulizong <a1207988466@qq.com>
2019-08-26 17:17:31 +0800

    抽象工厂模式

---
+++ Abstract_Factory/Abstract_Factory/AbstractFactory.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract_Factory
+{
+
+    #region 抽象产品类  ===============
+    /// <summary>
+    /// 手机抽象类
+    /// </summary>
+    public abstract class Phone
+    {
+        public abstract  string CreatePhone();
+    }
+    /// <summary>
+    /// 耳机抽象类
+    /// </summary>
+    public abstract class Headset
+    {
+        public abstract string CreateHeadset();
+    }
+    /// <summary>
+    /// 充电器抽象类
+    /// </summary>
+    public abstract class Charger
+    {
+        public abstract string CreateCharger();
+    }
+    #endregion
+
+    #region 华为具体产品类  ===========
+    public class HuaweiPhone : Phone
+    {
+        public override string CreatePhone()
+        {
+            return "华为手机一号现世";
+        }
+    }
+    public class HuaweiHeadset : Headset
+    {
+        public override string CreateHeadset()
+        {
+            return "华为手机一号原配耳机";
+        }
+    }
+    public class HuaweiCharger : Charger
+    {
+        public override string CreateCharger()
+        {
+            return "华为手机一号原配充电器";
+        }
+    }
+    #endregion
+
+    #region 小米具体产品类  =========== 
+    public class XiaomiPhone : Phone
+    {
+        public override string CreatePhone()
+        {
+            return "小米手机一号现世";
+        }
+    }
+    public class XiaomiHeadset : Headset
+    {
+        public override string CreateHeadset()
+        {
+            return "小米手机一号原配耳机";
+        }
+    }
+    public class XiaomiCharger : Charger
+    {
+        public override string CreateCharger()
+        {
+            return "小米手机一号原配充电器";
+        }
+    }
+    #endregion
+
+    #region 抽象工厂类  ===============
+    /// <summary>
+    /// 抽象工厂类
+    /// </summary>
+    public abstract class AbstractFactory
+    {
+        public abstract Phone GetPhone();
+        public abstract Headset GetHeadset();
+        public abstract Charger GetCharger();
+    }
+    #endregion
+
+    #region 华为具体工厂===============
+    public class HuaweiFactory : AbstractFactory
+    {
+
+        public override Phone GetPhone()
+        {
+            return new HuaweiPhone();
+        }
+
+        public override Headset GetHeadset()
+        {
+            return new HuaweiHeadset();
+        }
+         
+        public override Charger GetCharger()
+        {
+            return new HuaweiCharger();
+        }
+    }
+    #endregion
+
+    #region 小米具体工厂===============
+    public class XiaomiFactory : AbstractFactory
+    {
+
+        public override Phone GetPhone()
+        {
+            return new XiaomiPhone();
+        }
+
+        public override Headset GetHeadset()
+        {
+            return new XiaomiHeadset();
+        }
+
+        public override Charger GetCharger()
+        {
+            return new XiaomiCharger();
+        }
+    }
+    #endregion
+
+    #region 新增魅族手机需求===========
+    public class MeizuPhone : Phone
+    {
+        public override string CreatePhone()
+        {
+            return "魅族手机一号现世";
+        }
+    }
+    public class MeizuHeadset : Headset
+    {
+        public override string CreateHeadset()
+        {
+            return "魅族手机一号原配耳机";
+        }
+    }
+    public class MeizuCharger : Charger
+    {
+        public override string CreateCharger()
+        {
+            return "魅族手机一号原配充电器";
+        }
+    }
+    public class MeizuFactory : AbstractFactory
+    { 
+        public override Phone GetPhone()
+        {
+            return new MeizuPhone();
+        }
+        public override Headset GetHeadset()
+        {
+            return new MeizuHeadset();
+        }
+        public override Charger GetCharger()
+        {
+            return new MeizuCharger();
+        }
+
+    }
+    #endregion
+}
---
+++ Abstract_Factory/Abstract_Factory/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Abstract_Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///华为工厂生产华为产品
+            HuaweiFactory huaweiFactory = new HuaweiFactory();
+            var phone = huaweiFactory.GetPhone().CreatePhone();
+            var headset = huaweiFactory.GetHeadset().CreateHeadset();
+            var charger = huaweiFactory.GetCharger().CreateCharger();
+            Console.WriteLine("华为产品生产：" + phone + "——" + headset + "——" + charger);
+
+
+            ///小米工厂生产小米产品
+            XiaomiFactory xiaomiFactory = new XiaomiFactory();
+            phone = xiaomiFactory.GetPhone().CreatePhone();
+            headset = xiaomiFactory.GetHeadset().CreateHeadset();
+            charger = xiaomiFactory.GetCharger().CreateCharger();
+            Console.WriteLine("小米产品生产：" + phone + "——" + headset + "——" + charger);
+
+            ///魅族工厂生产小米产品
+            MeizuFactory meizuFactory = new MeizuFactory();
+            phone = meizuFactory.GetPhone().CreatePhone();
+            headset = meizuFactory.GetHeadset().CreateHeadset();
+            charger = meizuFactory.GetCharger().CreateCharger();
+            Console.WriteLine("魅族产品生产：" + phone + "——" + headset + "——" + charger);
+
+            Console.ReadLine();
+        }
+    }
+}
