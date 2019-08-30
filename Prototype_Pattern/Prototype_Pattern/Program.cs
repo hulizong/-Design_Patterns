c09030436bda258123ca75fc3fd296ed7ff76512
hulizong <a1207988466@qq.com>
2019-08-30 14:45:15 +0800

    原型模式

---
+++ Prototype_Pattern/Prototype_Pattern/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Prototype_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //学习
+            Prototype learnPrototype = new LearnPrototype();
+            learnPrototype.Status();
+            learnPrototype.Action("一号在学习：语文");
+
+            Prototype learnPrototype1 = learnPrototype.Clone();
+            learnPrototype.Status();
+            learnPrototype.Action("二号在学习：数学");
+
+            //玩耍
+            Prototype playPrototype = new PlayPrototype();
+            playPrototype.Status();
+            playPrototype.Action("一号在玩耍：游戏王");
+
+            Prototype playPrototype1 = playPrototype.Clone();
+            learnPrototype.Status();
+            learnPrototype.Action("二号在玩耍：四驱赛车");
+
+        }
+    }
+}
---
+++ Prototype_Pattern/Prototype_Pattern/PrototypePattern.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype_Pattern
+{
+    class PrototypePattern
+    {
+    }
+    #region 抽象原型类===========
+    public abstract class Prototype 
+    {
+        /// <summary>
+        /// 当前自己的状态
+        /// </summary>
+        public abstract void Status();
+        /// <summary>
+        /// 当前自己在做的事情
+        /// </summary>
+        public abstract void Action(string things);
+        /// <summary>
+        /// 拷贝自身的方法
+        /// </summary>
+        /// <returns></returns>
+        public abstract Prototype Clone();
+    }
+    #endregion
+
+    #region  具体原型类——读书学习 =============
+    public class LearnPrototype : Prototype
+    {
+        public override void Status()
+        {
+           Console.WriteLine("当前状态：好好学习"); 
+        }
+
+        public override void Action(string things)
+        {
+            Console.WriteLine(things);
+        }
+
+        public override Prototype Clone()
+        {
+            return (LearnPrototype)this.MemberwiseClone(); 
+        } 
+       
+    }
+    #endregion
+
+    #region  具体原型类——外出玩耍 =============
+    public class PlayPrototype : Prototype
+    { 
+        public override void Status()
+        {
+            Console.WriteLine("当前状态：开心玩耍");
+        }
+
+        public override void Action(string things)
+        {
+            Console.WriteLine( things);
+        }
+
+        public override Prototype Clone()
+        {
+            return (PlayPrototype)this.MemberwiseClone();
+        }
+    }
+    #endregion
+}
