9f03d06a288a55ea93607684f2e17e644f1ba2cf
hulizong <a1207988466@qq.com>
2019-09-11 16:47:37 +0800

    外观模式

---
+++ Facade_Pattern/Facade_Pattern/FacadePattern.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facade_Pattern
+{ 
+
+    #region 外观角色
+    public class FacadePattern
+    {
+        private RegisteredClass registeredClass;
+        private LoginClass loginClass;
+        private SendClass sendClass;
+        public FacadePattern() 
+        {
+             registeredClass = new RegisteredClass();
+            loginClass = new LoginClass();
+            sendClass = new SendClass();
+        }
+        public  void LoginFirst() 
+        {
+            registeredClass.Registered();
+            loginClass.Login();
+            sendClass.Send();
+        }
+    }
+    #endregion
+
+    #region 子系统
+    public class RegisteredClass 
+    {
+        public  void Registered() 
+        {
+            Console.WriteLine("注册成功");
+        }
+    }
+    public class LoginClass 
+    {
+        public  void Login()
+        {
+            Console.WriteLine("登录成功");
+        }
+    }
+    public class SendClass 
+    {
+        public  void Send()
+        {
+            Console.WriteLine("赠送成功");
+        }
+    }
+    #endregion
+}
---
+++ Facade_Pattern/Facade_Pattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Facade_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //第一次注册登录
+            FacadePattern facadePattern = new FacadePattern();
+            facadePattern.LoginFirst();
+             
+        }
+    }
+}
