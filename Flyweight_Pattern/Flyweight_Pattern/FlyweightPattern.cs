f100ec9b7829d9de9c5ed7eea563006d2764e13c
hulizong <a1207988466@qq.com>
2019-09-11 16:48:38 +0800

    享元模式

---
+++ Flyweight_Pattern/Flyweight_Pattern/FlyweightPattern.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Flyweight_Pattern
+{
+    class FlyweightPattern
+    {
+    }
+
+    #region 抽象享元角色——抽象公共接口
+    public abstract class Flyweight 
+    {
+        /// <summary>
+        /// 表述输出的位置
+        /// </summary>
+        /// <param name="externalstate">外在的状态，随着环境改变而改变</param>
+        public abstract void OutInput(int externalstate);
+    }
+    #endregion
+
+    #region 具体享元角色——实现其具体
+    public class SpecificFlyweight : Flyweight
+    {
+        private string Innerstate;
+        /// <summary>
+        /// 内部状态接收
+        /// </summary>
+        /// <param name="innerstate">内部状态</param>
+        public SpecificFlyweight(string innerstate) 
+        {
+            this.Innerstate = innerstate;
+        }
+        /// <summary>
+        /// 实现方法
+        /// </summary>
+        /// <param name="externalstate">外部状态</param>
+        public override void OutInput(int externalstate)
+        {
+            Console.WriteLine($"内部状态：{Innerstate}————外部状态：{externalstate}");
+        }
+    }
+    #endregion
+
+    #region 享元工厂角色——对享元角色进行创建及管理的
+    public class FlyweightFactory 
+    {
+        public Dictionary<string, SpecificFlyweight> keyValuePairs = new Dictionary<string, SpecificFlyweight>();
+
+        public SpecificFlyweight GetFlyweight(string Key) 
+        {
+            SpecificFlyweight specific = null;
+            if (!keyValuePairs.ContainsKey(Key))
+            {
+                Console.WriteLine("暂时未遇见该Key");
+                keyValuePairs.Add(Key, new SpecificFlyweight(Key));
+                Console.WriteLine("现已保存该Key"); 
+            }
+            else
+            {
+                 specific = keyValuePairs[Key] as SpecificFlyweight;
+            }
+            return specific;
+        }
+    }
+    #endregion
+}
---
+++ Flyweight_Pattern/Flyweight_Pattern/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Flyweight_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ///初始化享元工厂
+            FlyweightFactory factory = new FlyweightFactory();
+            while (true)
+            {
+                Console.WriteLine("请输入字符的位置：");
+                var indexstring = Console.ReadLine();
+                if (int.TryParse(indexstring, out int index))
+                {
+                    Console.WriteLine("请输入字符:");
+                    string str = Console.ReadLine();
+
+                    ///判断字符是否创建
+                    Flyweight flyweight = factory.GetFlyweight(str);
+
+                    //如果存在则输出信息
+                    if (flyweight != null)
+                    {
+                        flyweight.OutInput(index);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("请输入数字!");
+                }
+                Console.WriteLine("结束请输入N");
+                if (Console.ReadLine()=="N")
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("已结束");
+            Console.ReadLine();
+        }
+    }
+}
