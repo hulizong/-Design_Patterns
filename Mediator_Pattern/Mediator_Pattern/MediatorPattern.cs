d9fbee2f598cc9ed1e2f5d669c4b459d0d697970
hulizong <a1207988466@qq.com>
2019-10-11 17:59:23 +0800

    中介者模式

---
+++ Mediator_Pattern/Mediator_Pattern/MediatorPattern.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mediator_Pattern
+{
+    class MediatorPattern
+    {
+    }
+
+    /// <summary>
+    /// 抽象中介者
+    /// </summary>
+    public abstract class Mediator
+    {
+        /// <summary>
+        /// 定义与同事间的交互通信 
+        /// </summary>
+        public abstract void Common(string type);
+    }
+
+    /// <summary>
+    /// 抽象同事类
+    /// </summary>
+    public abstract class Colleague
+    {
+         
+
+        /// <summary>
+        /// 处理自己的事务（房东展示自己的房屋）
+        /// </summary>
+        public abstract void Handle();
+
+    }
+
+    /// <summary>
+    /// 房屋中介
+    /// </summary>
+    public class HouseMediator : Mediator
+    {
+        /// <summary>
+        /// 中介有所有房东的房屋信息
+        /// </summary>
+        private SmallHouseColleague SmallHouse;
+        private TwoHouseColleague TwoHouse;
+        private ThreeHouseColleague ThreeHouse;
+
+        public void SetSmallHouse(SmallHouseColleague small) 
+        {
+            SmallHouse = small;
+        }
+
+        public void SetTwoHouse(TwoHouseColleague two)
+        {
+            TwoHouse = two;
+        }
+        public void SetThreeHouse(ThreeHouseColleague three)
+        {
+            ThreeHouse = three;
+        }
+        /// <summary>
+        ///  
+        /// </summary>
+        /// <param name="colleague"></param>
+        public override void Common(string  type)
+        { 
+            switch (type)
+            {
+                case "单间":
+                    SmallHouse.Handle();
+                    Console.WriteLine("如果可以就可以租房了!");
+                    break;
+                case "两居室":
+                    TwoHouse.Handle();
+                    Console.WriteLine("如果可以就可以租房了!");
+                    break;
+                case "三居室":
+                    ThreeHouse.Handle();
+                    Console.WriteLine("如果可以就可以租房了!");
+                    break;
+                default: 
+                    Console.WriteLine($"{type}暂时没有房源!");
+                    break;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 房东（小房间）
+    /// </summary>
+    public class SmallHouseColleague : Colleague
+    {
+         
+
+        /// <summary>
+        /// 展示单间
+        /// </summary>
+        public override void Handle()
+        {
+            Console.WriteLine( "单间——便宜整洁");
+        }
+    }
+
+    /// <summary>
+    /// 房东（两居室）
+    /// </summary>
+    public class TwoHouseColleague : Colleague
+    { 
+
+        /// <summary>
+        /// 展示两居室
+        /// </summary>
+        public override void Handle()
+        {
+            Console.WriteLine("两居室——合适，靠谱");
+        }
+    }
+
+    /// <summary>
+    /// 房东（三居室）
+    /// </summary>
+    public class ThreeHouseColleague : Colleague
+    { 
+
+        /// <summary>
+        /// 展示三居室
+        /// </summary>
+        public override void Handle()
+        {
+            Console.WriteLine("三居室——大气，宽松");
+        }
+    }
+}
---
+++ Mediator_Pattern/Mediator_Pattern/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Mediator_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("一个租客看房：");
+            ///初始化中介
+            HouseMediator mediator = new HouseMediator();
+
+            ///初始化房屋信息
+            SmallHouseColleague smallHouseColleague = new SmallHouseColleague( );
+            TwoHouseColleague twoHouseColleague = new TwoHouseColleague( );
+            ThreeHouseColleague threeHouseColleague = new ThreeHouseColleague( );
+
+            ///中介获取房屋信息
+            mediator.SetSmallHouse(smallHouseColleague);
+            mediator.SetTwoHouse(twoHouseColleague);
+            mediator.SetThreeHouse(threeHouseColleague); 
+
+            ///租户A需要两居室、提供看房
+            mediator.Common("两居室");
+
+            //租户B需要四居室、暂无房源
+            mediator.Common("四居室");
+
+        }
+    }
+}
