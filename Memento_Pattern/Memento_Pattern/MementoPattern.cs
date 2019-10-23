81efb826700d72b0c9d9a662c73046f21e6a61fa
hulizong <a1207988466@qq.com>
2019-10-23 17:01:15 +0800

    备忘录模式

---
+++ Memento_Pattern/Memento_Pattern/MementoPattern.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Memento_Pattern
+{
+    class MementoPattern
+    {
+    }
+    #region 照片数据
+
+    public class Photo 
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 地址
+        /// </summary>
+        public string Address { get; set; }
+    }
+    #endregion
+
+    #region  发起人角色
+
+    public sealed class Sponsor 
+    {
+        private List<Photo> _photo;
+        public Sponsor(List<Photo> photos) 
+        {
+            if (photos == null)
+                throw new Exception("请传入正确的数据源!");
+            this._photo = photos;
+        }
+        public List<Photo> GetPhotos
+        {
+            get { return this._photo; }
+            set { this._photo = value; }
+        }
+
+        /// <summary>
+        /// 创建备忘录，保存状态数据
+        /// </summary>
+        /// <returns></returns>
+        public Memento CreateMemento() 
+        {
+            return new Memento(new List<Photo>(this._photo));
+        }
+
+        /// <summary>
+        /// 获取备忘录数据、恢复状态数据
+        /// </summary>
+        /// <param name="memento"></param>
+        public void RestoreMemento(Memento memento) 
+        {
+            GetPhotos = memento._mementoList;
+        }
+
+        /// <summary>
+        /// 展示数据
+        /// </summary>
+        public void ShowPhoto() 
+        {
+            Console.WriteLine($"目前用有照片{GetPhotos.Count}张：");
+            foreach (var item in GetPhotos)
+            {
+                Console.WriteLine($"照片名称：{item.Name}。照片地址：{item.Address}");
+            }
+        }
+    }
+    #endregion
+
+    #region   备忘录
+    public sealed class Memento 
+    {
+        public List<Photo> _mementoList { get; private set; }
+
+        /// <summary>
+        /// 初始化存储数据
+        /// </summary>
+        /// <param name="MementoList"></param>
+        public Memento(List<Photo> MementoList)
+        {
+            this._mementoList = MementoList;
+        }
+
+    }
+    #endregion
+
+    #region  管理员
+    /// <summary>
+    /// 一个备忘录数据处理
+    /// </summary>
+    public sealed class MementoManager 
+    {
+        public Memento  memento { get; set; }
+    }
+
+    /// <summary>
+    /// 多个备忘录数据处理
+    /// </summary>
+    public sealed class MementoManagers
+    {
+        public Dictionary<string, Memento> mementoList { get; set; }
+        public MementoManagers() 
+        {
+            mementoList = new Dictionary<string, Memento>();
+        }
+    }
+    #endregion
+}
---
+++ Memento_Pattern/Memento_Pattern/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Memento_Pattern
+{
+    class Program
+    {
+
+        //static void Main(string[] args)
+        //{
+        //    ///初始化数据
+        //    List<Photo> photos = new List<Photo>();
+        //    photos.Add(new Photo { Name = "第一张.jpg", Address = "https://img2018.cnblogs.com/blog/1470432/201910/11.jpg" });
+        //    photos.Add(new Photo { Name = "第二张.jpg", Address = "https://img2018.cnblogs.com/blog/1470432/201910/22.jpg" });
+        //    photos.Add(new Photo { Name = "第三张.jpg", Address = "https://img2018.cnblogs.com/blog/1470432/201910/33.jpg" });
+        //    Sponsor sponsor = new Sponsor(photos);
+
+        //    ///展示数据
+        //    sponsor.ShowPhoto();
+
+        //    ///保存状态数据到备忘录
+        //    MementoManager mementoManager = new MementoManager();
+        //    mementoManager.memento = sponsor.CreateMemento();
+
+        //    ///删除一张照片
+        //    Console.WriteLine();
+        //    Console.WriteLine();
+        //    photos.RemoveAt(0);
+        //    sponsor.GetPhotos = photos;
+        //    Console.WriteLine("删除后");
+        //    sponsor.ShowPhoto();
+
+        //    ///恢复备忘录数据
+        //    ///
+        //    Console.WriteLine();
+        //    Console.WriteLine();
+        //    sponsor.RestoreMemento(mementoManager.memento);
+        //    Console.WriteLine("恢复后");
+        //    sponsor.ShowPhoto();
+
+        //}
+        static void Main(string[] args)
+        {
+            ///初始化数据
+            List<Photo> photos = new List<Photo>();
+            photos.Add(new Photo { Name = "第一张.jpg", Address = "https://img2018.cnblogs.com/blog/1470432/201910/11.jpg" });
+            photos.Add(new Photo { Name = "第二张.jpg", Address = "https://img2018.cnblogs.com/blog/1470432/201910/22.jpg" });
+            photos.Add(new Photo { Name = "第三张.jpg", Address = "https://img2018.cnblogs.com/blog/1470432/201910/33.jpg" });
+            Sponsor sponsor = new Sponsor(photos);
+
+            ///展示数据
+            sponsor.ShowPhoto();
+
+            ///保存状态数据到备忘录
+            MementoManagers mementoManagers = new MementoManagers();
+            mementoManagers.mementoList.Add("1", sponsor.CreateMemento()); 
+
+            ///删除一张照片
+            Console.WriteLine();
+            Console.WriteLine();
+            photos.RemoveAt(0);
+            sponsor.GetPhotos = photos;
+            Console.WriteLine("删除后");
+            sponsor.ShowPhoto();
+            mementoManagers.mementoList.Add("2", sponsor.CreateMemento());
+
+            ///恢复备忘录数据
+            ///
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine($"目前有{mementoManagers.mementoList.Count}个备份数据,请输入序号选择备份数据恢复");
+                var index = Console.ReadLine();
+                sponsor.RestoreMemento(mementoManagers.mementoList.GetValueOrDefault(index));
+                Console.WriteLine("恢复后");
+                sponsor.ShowPhoto();
+                Console.WriteLine("输入q退出");
+                var q = Console.ReadLine();
+                if (q=="q")
+                {
+                    break;
+                }
+            }
+           
+
+        }
+    }
+}
